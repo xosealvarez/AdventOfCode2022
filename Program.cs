7ba2e11aafa1b56bdd3bda127f6c2c7fd635fc3e
Jose Luis Alvarez <xosealvarezrodriguez@gmail.com>
2022-12-01 14:49:29 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode2022.Puzzles;
+
+namespace AdventOfCode2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PuzzlesDay1 puzzlesDay1 = new PuzzlesDay1(@"./Inputs/Tests/InputDay1.txt");
+            Console.WriteLine(puzzlesDay1.getPart2());
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Puzzles/PuzzlesDay1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Puzzles
+{
+    public class PuzzlesDay1
+    {
+        private readonly string[] _input;
+        private List<int> _sumCaloriesByElf = new List<int>();
+
+        public PuzzlesDay1(string inputFilePath)
+        {
+            _input = System.IO.File.ReadAllLines(inputFilePath);
+        }
+
+        public int getPart1()
+        {
+            int numCalories = 0;
+            
+            _sumCaloriesByElf.Add(0);
+
+            foreach (string line in _input)
+            {
+                if(line.Equals(string.Empty))
+                {
+                    _sumCaloriesByElf.Add(0);
+                }
+                else
+                {
+                    numCalories = Convert.ToInt32(line);
+                    _sumCaloriesByElf[_sumCaloriesByElf.Count-1] += numCalories;
+                }
+            }
+
+            _sumCaloriesByElf.Sort();
+            _sumCaloriesByElf.Reverse();
+
+            return _sumCaloriesByElf[0];
+        }
+
+        public int getPart2()
+        {
+            getPart1();
+            return _sumCaloriesByElf[0] + _sumCaloriesByElf[1] + _sumCaloriesByElf[2];
+        }
+    }
+}
